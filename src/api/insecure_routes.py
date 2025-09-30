d7bc17bfa5945f46f3a4c9542568263b5d7eb9ad
Bardin Petr <bardin.petr@gmail.com>
2025-09-30 20:44:59 +0300

    snyk enable code inspection

--- src/api/insecure_routes.py
+++ src/api/insecure_routes.py
@@ -6,28 +6,28 @@ from ..security import require_roles, generate_jwt_token
 insecure_bp = Blueprint('insecure', __name__, url_prefix='/insecure')
 
 
-# @insecure_bp.route('/login', methods=['POST'])
-# def insecure_sqli():
-#     """
-#     Intentionally vulnerable for SQLi
-#     Example:
-#         POST http://localhost:5000/insecure/login
-#         Body:
-#         {
-#             "username": "admin' or 'a'='a",
-#             "password": "invalid_password_haha_sqli"
-#         }
-#     """
-#     username = request.get_json().get('username')
-#
-#     phash = 'only_sql_i_can_pass--this_is_stub_for_password_has'
-#     query = f"SELECT id, username, password_hash FROM user WHERE password_hash = '{phash}' AND username = '{username}'"
-#     result = db.engine.connect().execute(db.text(query))
-#     result = result.fetchone()
-#     if result is not None:
-#         token = generate_jwt_token(result[0], result[1])
-#         return jsonify({"access_token": token}), 200
-#     return jsonify({"error": "err"}), 401
+@insecure_bp.route('/login', methods=['POST'])
+def insecure_sqli():
+    """
+    Intentionally vulnerable for SQLi
+    Example:
+        POST http://localhost:5000/insecure/login
+        Body:
+        {
+            "username": "admin' or 'a'='a",
+            "password": "invalid_password_haha_sqli"
+        }
+    """
+    username = request.get_json().get('username')
+
+    phash = 'only_sql_i_can_pass--this_is_stub_for_password_has'
+    query = f"SELECT id, username, password_hash FROM user WHERE password_hash = '{phash}' AND username = '{username}'"
+    result = db.engine.connect().execute(db.text(query))
+    result = result.fetchone()
+    if result is not None:
+        token = generate_jwt_token(result[0], result[1])
+        return jsonify({"access_token": token}), 200
+    return jsonify({"error": "err"}), 401
 
 
 @insecure_bp.route('/add', methods=['POST'])
